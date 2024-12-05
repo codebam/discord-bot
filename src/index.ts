a96e0249446c96a306e61c6c13ccf321116148ea
Sean Behan <codebam@riseup.net>
2024-12-04 22:27:53 -0500

    discord bot

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,17 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { verifyKey } from 'discord-interactions';
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const signature = request.headers.get('X-Signature-Ed25519') as string;
+		const timestamp = request.headers.get('X-Signature-Timestamp') as string;
+		const body = await request.text();
+		if (!(await verifyKey(body, signature, timestamp, '0b5b1993b65944d7262e91adfea6da4133112a0d1071c2bf899f8b95d86da6af'))) {
+			return new Response('Invalid request signature', { status: 401 });
+		}
+		const b = JSON.parse(body);
+		if (b.type === 1) {
+			return new Response(JSON.stringify({ type: 1 }), { headers: { 'Content-Type': 'application/json' } });
+		}
+		return new Response('Unhandled interaction type', { status: 400 });
 	},
 } satisfies ExportedHandler<Env>;
